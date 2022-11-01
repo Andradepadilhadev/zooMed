f4cbc5d088fa04a74ccf9b1ac6d10a4892d57fe2
Erica Lopes <ericalopes@outlook.com>
2022-11-01 18:33:19 -0300

    login/createLogin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import { Express } from "express";
+import { loginRoutes } from "./session.routes";
 
-const appRoutes = (app: Express) => {};
+const appRoutes = (app: Express) => {
+    app.use("/login", loginRoutes)
+    
+};
 
 export default appRoutes;
---
+++ src/services/session/login.services.ts
@@ -0,0 +1,36 @@
+import { AppError } from "../../errors/appError";
+import { IUserLogin } from "../../interfaces/users";
+import { usersRepository } from "../../utilities";
+import { compare } from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const loginService = async ({
+  email,
+  password,
+}: IUserLogin): Promise<string> => {
+  const user = await usersRepository.findOneBy({ email: email });
+
+  if (!user) {
+    throw new AppError("Invalid email or password", 403);
+  }
+
+  const matchPass = await compare(password, user.password);
+
+  if (!matchPass) {
+    throw new AppError("Invalid email or password", 403);
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.SECRET_KEY as string,
+    {
+      expiresIn: "24h",
+      subject: user.id,
+    }
+  );
+  return token;
+};
+
+export default loginService;
