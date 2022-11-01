805cbfe92124d0b917e36979415807536ed91dc9
enilson silva <enilsonsilva999@gmail.com>
2022-11-01 19:35:44 -0300

    feat:creat_delete_animals

--- src/middlewares/handleError.middlewares.ts
+++ src/middlewares/handleError.middlewares.ts
@@ -1,18 +1,21 @@
-import { Request, Response, NextFunction } from "express"
-import { AppError } from "../errors/appError"
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors/appError";
 
-const handleErrorMiddleware = async (error: Error, req: Request, res: Response, next: NextFunction) => {
-  
-    if(error instanceof AppError){
-        return res.status(error.statusCode).json({
-            message: error.message
-        })
-    }
-    
-    return res.status(500).json({
-        message: "Internal server error"
-    })
-    
-}
+const handleErrorMiddleware = async (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
 
-export default handleErrorMiddleware
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+};
+
+export default handleErrorMiddleware;
