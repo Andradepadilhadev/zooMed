d65aeb83ca35abeca188013035b60b592c720aa9
Erica Lopes <ericalopes@outlook.com>
2022-11-02 14:33:00 -0300

    feat/utils/verify

---
+++ src/interfaces/doctors/index.ts
@@ -0,0 +1,10 @@
+export interface IDoctorRequest {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+  crmv: object;
+  isActive: true;
+  speciality_id: string;
+  clinics_id: string;
+}
---
+++ src/utilities/verifyReviewDate.ts
@@ -0,0 +1,4 @@
+const verifyReviewDate = () => {};
+
+export { verifyReviewDate };
+//não pode ser criado antes da data da consulta
