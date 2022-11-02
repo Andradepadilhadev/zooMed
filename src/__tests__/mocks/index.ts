408d2dc537c74a894853f1126bd58c0cdbad3cb7
gabihcaldeira <gabihcaldeira@hotmail.com>
2022-11-02 16:03:12 -0300

    tests/create_users_routes_tests

---
+++ src/__tests__/mocks/index.ts
@@ -0,0 +1,13 @@
+export const mockedUser = {
+  name: "Kenzinho",
+  email: "kenzinho@email.com",
+  password: "1234",
+  birthDate: "1995/4/25",
+};
+
+export const mockedUserUpdated = {
+  email: "kenzie@mail.com",
+  birthDate: "2/14/1995",
+};
+
+export const mockedAnimal = {};
