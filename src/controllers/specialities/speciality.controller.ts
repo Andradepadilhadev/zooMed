0e5f9493a9829deae310be1b02cc3a9f8943816b
Caio S.G <caiosilvestre.dev@gmail.com>
2022-11-03 12:59:30 -0300

    Refatorei o local de algumas coisas padronizando o projeto

--- src/controllers/specialities/speciality.controller.ts
+++ src/controllers/specialities/speciality.controller.ts
@@ -1,7 +1,7 @@
 import listAllSpecialityService from "../../services/doctors/speciality/listAllSpeciality.service";
 import { Request, Response } from "express";
 import listDoctorBySpecialityService from "../../services/doctors/speciality/listDoctorBySpeciality.service";
-
+import specialitiesDeleteService from "../../services/doctors/speciality/specialitiesDelete.service";
 const listAllSpecController = async (req: Request, res: Response) => {
   const listSpecs = await listAllSpecialityService();
 
@@ -15,4 +15,17 @@ const listAllDocsBySpecsController = async (req: Request, res: Response) => {
   return res.json(listDocs);
 };
 
-export { listAllSpecController, listAllDocsBySpecsController };
+const specialitiesDeleteController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await specialitiesDeleteService(id);
+
+    return res.status(203).send();
+  } catch (error) {}
+};
+
+export {
+  listAllSpecController,
+  listAllDocsBySpecsController,
+  specialitiesDeleteController,
+};
