600dcc16503c1f9faa4552adb3e06267ef006c89
gabihcaldeira <gabihcaldeira@hotmail.com>
2022-11-04 00:45:00 -0300

    tests/create_appointments_tests

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -7,14 +7,14 @@ export const mockedUser = {
 
 export const mockedUserUpdated = {
   email: "kenzie@mail.com",
-  birthDate: "2/14/1995",
+  birthDate: "1995/2/14",
 };
 
-export const mockedType = {
+export const mockedSpecies = {
   name: "Gato",
 };
 
-export const mockedTypeUpdated = {
+export const mockedSpeciesUpdated = {
   name: "Rato",
 };
 
@@ -22,7 +22,7 @@ export let mockedAnimal = {
   name: "Bob",
   birthDate: "2020/12/6",
   breed: "sem raça definida",
-  type: "",
+  species: "",
 };
 
 export const mockedWrongId = "130236f0-4e0c-40e0-9000-3d68dcc6f9a9";
@@ -33,4 +33,45 @@ export const mockedDoctor = {
   password: "1234",
   birthDate: "1988/07/19",
   crmv: "123456",
+};
+
+export const mockedSpeciality = {
+  name: "Cardiologista",
+};
+
+export const mockedClinic = {
+  name: "Clínica Vet",
+  contact: "27123456789",
+  address: {
+    zipCode: "12345678",
+    number: "100",
+    complement: "sala 2",
+    district: "Bairro",
+    city: "Cidade",
+    state: "ES",
+  },
+};
+
+export const mockedClinicTwo = {
+  name: "ZooClínica",
+  contact: "31987654321",
+  address: {
+    zipCode: "87654321",
+    number: "3",
+    district: "Outro Bairro",
+    city: "Outra Cidade",
+    state: "RJ",
+  },
+};
+
+export const mockedAppointment = {
+  date: "2020/11/7",
+  hour: "10:30",
+  animalsId: "",
+  clinicsDoctorsId: "",
+};
+
+export const mockedReview = {
+  review: "Avaliação da consulta",
+  appointmentId: "",
 };
