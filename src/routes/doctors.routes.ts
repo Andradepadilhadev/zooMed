f534cb05bc259e55d0e4e1937b9befc0a60bf309
byetevinn <stevan14padilha@gmail.com>
2022-11-05 18:39:26 -0300

    develop/fix_doctor_routes

--- src/routes/doctors.routes.ts
+++ src/routes/doctors.routes.ts
@@ -1,21 +1,8 @@
 import { Router } from "express";
 import deleteAppointmentDoctorController from "../controllers/doctors/appointments/deleteAppointmentsDoctor.controller";
 import listAppointmentsDoctorController from "../controllers/doctors/appointments/listAppointmentsDoctor.controller";
-import createDoctorController from "../controllers/doctors/createDoctor.controller";
-import deleteDoctorController from "../controllers/doctors/deleteDoctor.controller";
-import listDoctorsController from "../controllers/doctors/listDoctors.controller";
-import listReviewsDoctorController from "../controllers/doctors/reviews/listReviewsDoctor.controller";
-import updateDoctorController from "../controllers/doctors/updateDoctor.controller";
-import {
-  createDoctorController,
-  deleteDoctorController,
-  listDoctorsController,
-  updateDoctorController,
-} from "../controllers/doctors/doctors.controller";
-import {
-  deleteReviewsController,
-  listReviewsDoctorController,
-} from "../controllers/doctors/reviews/reviews.controller";
+import { createDoctorController, deleteDoctorController, listDoctorsController, updateDoctorController } from "../controllers/doctors/doctors.controller";
+import { listReviewsDoctorController } from "../controllers/doctors/reviews/reviews.controller";
 import { createSpecialityController } from "../controllers/specialities/speciality.controller";
 import ensureAuthTokenMiddleware from "../middlewares/ensureAuthToken.middleware";
 import ensureDoctorMiddleware from "../middlewares/ensureDoctor.middleware";
