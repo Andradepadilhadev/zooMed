86b212e10f0287e13d272ce46a80315b8d317939
gabihcaldeira <gabihcaldeira@hotmail.com>
2022-11-08 14:22:53 -0300

    docker/remove_dist

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,6 +12,7 @@ const AppDataSource = new DataSource(
     : {
         type: "postgres",
         host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
         database: process.env.DB,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
